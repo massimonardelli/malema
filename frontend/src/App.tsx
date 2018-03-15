a771ad836736d07acac15d08a0503d3ffcf09067
Matteo Marianecci <mattonerd@gmail.com>
2018-03-15 09:56:19 +0100

    React Typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+
+const logo = require('./logo.svg');
 
 class App extends React.Component {
 	render() {
-		
-		this.name = 'Ciao';
-		
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo"/>
-					<h1 className="App-title">{this.name}</h1>
+					<h1 className="App-title">Welcome to React</h1>
 				</header>
 				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
+					To get started, edit <code>src/App.tsx</code> and save to reload.
 				</p>
 			</div>
 		);
